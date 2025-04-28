4447839e842597228a92361a53fc17517273c5e5
Lior Zemah <liorzemah2@gmail.com>
2025-04-28 21:59:32 +0300

    Permission hierarchy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,18 +13,21 @@ const Navbar = () => {
     <>
      {showLogin?<LoginPopup setShowLogin={setShowLogin} />: <></>}
     <header>
-         <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-5 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
-        <Image
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-5 px-6 py-4 bg-transparent'>
+        <Link href='/' className='flex justify-center items-center'>
+          <Image
           src='/logo (1).svg'
           alt='logo'
           width={150}
           height={20}
           className='object-contain'
-        />
-      </Link>
-      <Link href='/LoginPopup' className='text-center text-white rounded-full bg-black min-w-[150px] min-h-[30px] hover:bg-cyan-900 focus-within:shadow-lg'>
-        Sign In</Link>
+          />
+        </Link>
+        <Link
+          href='/LoginPopup'
+          className='text-center text-white rounded-full bg-black min-w-[150px] min-h-[30px] hover:bg-cyan-900 focus-within:shadow-lg'>
+          Sign In
+        </Link>
       </nav>
     </header>
     </>
